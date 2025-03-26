641f758b5ccf231c94baf19320783e25a28ce130
StayFar <144968807+StayFar@users.noreply.github.com>
2025-03-26 23:22:39 +0000

    edit button size

--- fab.js
+++ fab.js
@@ -44,13 +44,17 @@ document.querySelector("body").insertAdjacentHTML("beforeend", `
         bottom: 1em;
         right: 1em;
     }
+    #fab${suffix} {
+        font-size: 0.8em;
+        padding: 0.3em 0.6em;
+    }
     #clear${suffix}{
         position: absolute;
         top: -1em;
         right: 1em;
     }
     </style>
-    <button id="fab${suffix}">Tell me</button>
+    <button id="fab${suffix}">Order Here</button>
     `);
 
 document.querySelector(`#fab${suffix}`).addEventListener("click", evt=>{
