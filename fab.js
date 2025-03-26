e42232b53383125400c67b4fabaccd1e4322bba2
StayFar <144968807+StayFar@users.noreply.github.com>
2025-03-26 21:59:44 +0000

    fix position

--- fab.js
+++ fab.js
@@ -6,7 +6,7 @@ class Blur {
         this.div.id = "blurred_background";
         this.div.innerHTML = `<style>
         #blurred_background{
-            position:absolute;
+            position: fixed;
             top:0;
             left:0;
             height:100vh;
@@ -40,7 +40,7 @@ const suffix = (Math.random()*100).toFixed().toString();
 document.querySelector("body").insertAdjacentHTML("beforeend", `
     <style>
     #fab${suffix}, #modal${suffix}{
-        position: absolute;
+        position: fixed;
         bottom: 1em;
         right: 1em;
     }
@@ -49,7 +49,7 @@ document.querySelector("body").insertAdjacentHTML("beforeend", `
         padding: 0.3em 0.6em;
     }
     #clear${suffix}{
-        position: absolute;
+        position: fixed;
         top: -1em;
         right: 1em;
     }
