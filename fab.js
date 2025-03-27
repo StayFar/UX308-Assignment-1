489777c3388f7b4704a66f1e101bd5a1efca2286
StayFar <144968807+StayFar@users.noreply.github.com>
2025-03-27 20:05:41 +0000

    static button?

--- fab.js
+++ fab.js
@@ -6,61 +6,45 @@ class Blur {
         this.div.id = "blurred_background";
         this.div.innerHTML = `<style>
         #blurred_background{
-            position:absolute;
-            top:0;
-            left:0;
-            height:100vh;
-            width:100vw;
+            position: fixed;
+            top: 0;
+            left: 0;
+            height: 100vh;
+            width: 100vw;
             backdrop-filter: blur(8px);
-            z-index:1001;
-        }
-        #blurred_background p{
-            display: -webkit-box;
-            -webkit-line-clamp: 3;
-            line-clamp: 3;
-            overflow: hidden;
-            text-overflow: ellipsis;
-            -webkit-box-orient: vertical;
-        }
-        #blurred_background div{
-            padding: 0 1em;
+            z-index: 1001;
         }
         </style>
         <div>${message}</div>`;
         oBody?.insertAdjacentElement("afterbegin", this.div);
     }
-    close(){
+    close() {
         this.div.remove();
     }
- }
- 
- 
- 
- 
- const suffix = (Math.random()*100).toFixed().toString();
- 
- 
- document.querySelector("body").insertAdjacentHTML("beforeend", `
+}
+
+const suffix = (Math.random() * 100).toFixed().toString();
+
+document.querySelector("body").insertAdjacentHTML("beforeend", `
     <style>
-    #fab${suffix}, #modal${suffix}{
-        position: absolute;
+    #fab${suffix}, #modal${suffix} {
+        position: fixed;
         bottom: 1em;
         right: 1em;
         font-size: 0.8em;
         padding: 0.3em 0.6em;
+        z-index: 1002;
     }
-    #clear${suffix}{
+    #clear${suffix} {
         position: absolute;
         top: -1em;
         right: 1em;
     }
     </style>
     <button id="fab${suffix}">Order Here</button>
-    `);
- 
- 
- document.querySelector(`#fab${suffix}`).addEventListener("click", evt=>{
+`);
+
+document.querySelector(`#fab${suffix}`).addEventListener("click", evt => {
     const blur = new Blur(`<div id="modal${suffix}"><div><x-chat /></div><button id="clear${suffix}">clear</button></div>`);
     document.querySelector(`#clear${suffix}`).addEventListener("click", () => blur.close());
- });
- 
+});
